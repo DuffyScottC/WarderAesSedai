5d0470d97cb355c17be2acec4e4f7c04cdb92b53
Scott Duffy <duffyscottc@gmail.com>
2021-11-04 17:38:05 -0400

    cancels all entity-to-entity damage

--- src/main/java/me/braekpo1nt/warderaessedai/Main.java
+++ src/main/java/me/braekpo1nt/warderaessedai/Main.java
@@ -2,6 +2,7 @@ package me.braekpo1nt.warderaessedai;
 
 import me.braekpo1nt.warderaessedai.commands.WarderBondCommand;
 import me.braekpo1nt.warderaessedai.listeners.AesSedaiListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
@@ -9,7 +10,8 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         new WarderBondCommand(this);
-        
+
+        Bukkit.getLogger().info("listener created");
         new AesSedaiListener(this);
     }
 
--- src/main/java/me/braekpo1nt/warderaessedai/listeners/AesSedaiListener.java
+++ src/main/java/me/braekpo1nt/warderaessedai/listeners/AesSedaiListener.java
@@ -1,17 +1,20 @@
 package me.braekpo1nt.warderaessedai.listeners;
 
 import me.braekpo1nt.warderaessedai.Main;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class AesSedaiListener implements Listener {
     public AesSedaiListener(Main plugin) {
+        Bukkit.getLogger().info("listener registered");
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
     
     @EventHandler
     public void onPlayerDamageEntity(EntityDamageByEntityEvent event) {
-        
+        Bukkit.getLogger().info("event cancelled");
+        event.setCancelled(true);
     }
 }
