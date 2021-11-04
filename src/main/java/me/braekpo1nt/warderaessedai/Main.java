db633bc67652897d1d1696750ce493ac54c4c7bf
Scott Duffy <duffyscottc@gmail.com>
2021-11-04 17:21:14 -0400

    added command and listener

---
+++ src/main/java/me/braekpo1nt/warderaessedai/Main.java
@@ -0,0 +1,20 @@
+package me.braekpo1nt.warderaessedai;
+
+import me.braekpo1nt.warderaessedai.commands.WarderBondCommand;
+import me.braekpo1nt.warderaessedai.listeners.AesSedaiListener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        new WarderBondCommand(this);
+        
+        new AesSedaiListener(this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/me/braekpo1nt/warderaessedai/listeners/AesSedaiListener.java
@@ -0,0 +1,17 @@
+package me.braekpo1nt.warderaessedai.listeners;
+
+import me.braekpo1nt.warderaessedai.Main;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+public class AesSedaiListener implements Listener {
+    public AesSedaiListener(Main plugin) {
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+    
+    @EventHandler
+    public void onPlayerDamageEntity(EntityDamageByEntityEvent event) {
+        
+    }
+}
