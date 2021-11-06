2bb2c78d9adcc295fddbeef95487822f6152a2ff
Scott Duffy <duffyscottc@gmail.com>
2021-11-06 11:35:34 -0400

    added warder and aes sedai to plugin

--- src/main/java/me/braekpo1nt/warderaessedai/Main.java
+++ src/main/java/me/braekpo1nt/warderaessedai/Main.java
@@ -1,18 +1,39 @@
 package me.braekpo1nt.warderaessedai;
 
-import me.braekpo1nt.warderaessedai.commands.WarderBondCommand;
+import me.braekpo1nt.warderaessedai.commands.subcommands.BondCommand;
 import me.braekpo1nt.warderaessedai.listeners.AesSedaiListener;
-import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
-
+    
+    private Player warder;
+    private Player aesSedai;
+    
     @Override
     public void onEnable() {
-        new WarderBondCommand(this);
+        new BondCommand(this);
 
         new AesSedaiListener(this);
     }
+
+
+    public Player getWarder() {
+        return warder;
+    }
+
+    public void setWarder(Player warder) {
+        this.warder = warder;
+    }
+
+    public Player getAesSedai() {
+        return aesSedai;
+    }
+
+    public void setAesSedai(Player aesSedai) {
+        this.aesSedai = aesSedai;
+    }
+
 
     @Override
     public void onDisable() {
--- src/main/java/me/braekpo1nt/warderaessedai/listeners/AesSedaiListener.java
+++ src/main/java/me/braekpo1nt/warderaessedai/listeners/AesSedaiListener.java
@@ -7,12 +7,18 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class AesSedaiListener implements Listener {
+    
+    private Main plugin;
+    
     public AesSedaiListener(Main plugin) {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        this.plugin = plugin;
     }
     
     @EventHandler
     public void onPlayerDamageEntity(EntityDamageByEntityEvent event) {
-        event.setCancelled(true);
+        if (event.getDamager().equals(plugin.getAesSedai())) {
+            event.setCancelled(true);
+        }
     }
 }
