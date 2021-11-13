cfa066910f4b34c08f66f2ff739af83d4c83c41c
Scott Duffy <duffyscottc@gmail.com>
2021-11-13 12:30:42 -0500

    added boss bar from tutorial https://www.youtube.com/watch?v=9apL86E05Bc

--- src/main/java/me/braekpo1nt/warderaessedai/Main.java
+++ src/main/java/me/braekpo1nt/warderaessedai/Main.java
@@ -1,17 +1,33 @@
 package me.braekpo1nt.warderaessedai;
 
 import me.braekpo1nt.warderaessedai.commands.subcommands.BondSubCommand;
+import me.braekpo1nt.warderaessedai.compass.Bar;
 import me.braekpo1nt.warderaessedai.listeners.AesSedaiListener;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Main extends JavaPlugin {
+public final class Main extends JavaPlugin implements Listener {
     
     private Player warder;
     private Player aesSedai;
+    public Bar bar;
     
     @Override
     public void onEnable() {
+        this.getServer().getPluginManager().registerEvents(this, this);
+        bar = new Bar(this);
+        bar.createBar();
+        
+        if (Bukkit.getOnlinePlayers().size() > 0) {
+            for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                bar.addPlayer(onlinePlayer);
+            }
+        }
+        
         new BondSubCommand(this);
 
         new AesSedaiListener(this);
@@ -38,5 +54,12 @@ public final class Main extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+    
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        if (!bar.getBar().getPlayers().contains(event.getPlayer())) {
+            bar.addPlayer((event.getPlayer()));
+        }
     }
 }
